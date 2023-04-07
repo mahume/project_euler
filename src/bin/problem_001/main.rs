ddd84b6d950f41b44c3b39bd906a5924342d8dfa
Mike Hume <michael.a.hume@pm.me>
2023-04-07 16:59:17 -0400

    Problem 1 passed

--- src/bin/problem_001/main.rs
+++ src/bin/problem_001/main.rs
@@ -1,14 +1,45 @@
+use std::error;
+use std::ops::Range;
+
 /// Problem: [Multiples of 3 or 5](https://projecteuler.net/problem=1)
 ///
 /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 /// Find the sum of all the multiples of 3 or 5 below 1000.
 
-fn main() {
-    println!("Hello, Euler!")
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let natural_numbers_list = 0..1000;
+    let multiples = vec![3, 5];
+
+    let r = Ranger::new(natural_numbers_list, multiples);
+
+    println!("Sum of multiples: {}", r.sum_of_multiples());
+
+    Ok(())
 }
 
-fn solve() -> i32 {
-    0
+struct Ranger {
+    natural_numbers: Range<i32>,
+    multiples: Vec<i32>,
+}
+
+impl Ranger {
+    fn new(natural_numbers: Range<i32>, multiples: Vec<i32>) -> Self {
+        Self {
+            natural_numbers,
+            multiples,
+        }
+    }
+
+    fn multiples(&self) -> Vec<i32> {
+        self.natural_numbers
+            .clone()
+            .filter(|nn| *nn != 0 && self.multiples.iter().any(|m| nn % m == 0))
+            .collect()
+    }
+
+    fn sum_of_multiples(&self) -> i32 {
+        self.multiples().iter().sum()
+    }
 }
 
 #[cfg(test)]
@@ -16,12 +47,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
-        let mock_range = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    fn all_multiples() {
+        let r = Ranger::new(0..10, vec![3, 5]);
 
-        let multiples = vec![3, 5];
-        let multiples_of = vec![3, 5, 6, 9];
+        assert_eq!(r.multiples(), vec![3, 5, 6, 9]);
+    }
 
-        assert_eq!(solve(mock_range), 23);
+    #[test]
+    fn sum_of_multiples() {
+        let r = Ranger::new(0..10, vec![3, 5]);
+
+        assert_eq!(r.sum_of_multiples(), 23);
     }
 }
